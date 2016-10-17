b74bcdb224bb4da03c7a9d475e095ed075929072
fgali <f.gali.18@gmail.com>
2016-10-16 22:05:51 -0400

    Cambion en la forma de retornar los datos json en el api.

--- src/Controllers/UsuariosController.php
+++ src/Controllers/UsuariosController.php
@@ -15,32 +15,25 @@ class UsuariosController extends BaseController {
 	public function get($request, $response) {
 		$usuario = new Usuario($this->container);
 		$list = $usuario->select('*');
-		$response->withHeader('Content-Type', 'application/json');
-        $response->write(json_encode($list));
-        return $response;
+		return $response->withJson($list, 201, $encodingOptions);
 	}
 
 	public function getId($request, $response, $args) {
 		$id = $args['id'];
-
 		$usuario = new Usuario($this->container);
 		$list = $usuario->select('*', ["usu_id[=]" => $id]);
-		$response->withHeader('Content-Type', 'application/json');
-        $response->write(json_encode($list));
-        return $response;
+		return $response->withJson($list, 201, $encodingOptions);
 	}
 
 	public function guardar($request, $response, $args) {
 		$list = 'Guardar Usuario';
 		$response->withHeader('Content-Type', 'application/json');
         $response->write(json_encode($list));
-        return $response;
+        return $response->withJson([$list], 201, $encodingOptions);
 	}
 
 	public function eliminar($request, $response, $args) {
 		$list = 'Eliminar Usuario: '.$args['id'];
-		$response->withHeader('Content-Type', 'application/json');
-        $response->write(json_encode($list));
-        return $response;
+		return $response->withJson([$list], 201, $encodingOptions);
 	}
 }
